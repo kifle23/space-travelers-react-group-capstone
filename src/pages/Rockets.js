e608ec2eb7098d4855fb015ca1b0788c1408dfb7
katfogy <katfogy@gmail.com>
2023-06-20 09:51:19 +0100

    single rocket styling done

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,5 +1,21 @@
-const Rockets = () => (
-  <div className="container"><h2>Rocket</h2></div>
-);
+import image from '../falcon.jpg';
+
+const Rockets = () => {
+  const style = {
+    width: 200,
+  };
+  return (
+    <div className="container">
+      <div className=" d-flex-space g-10 my-20">
+        <img src={image} width={style.width} alt="rocket" />
+        <div className="rocket-details">
+          <h2 className="rocket-name">Falcon 1</h2>
+          <p className="rocket-desc">orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <button type="button" className="rocket-reserve btn">Reserve Rocket</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Rockets;

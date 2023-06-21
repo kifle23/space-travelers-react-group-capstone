a955d5262949c32631e54d8a8ec7422d481786ef
katfogy <katfogy@gmail.com>
2023-06-21 08:57:45 +0100

    code refactor

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../redux/features/Rockets/RocketSlice';
+import Rocket from '../components/Rocket';
 
 const Rockets = () => {
   const rockets = useSelector((store) => store.rockets);
@@ -18,15 +19,14 @@ const Rockets = () => {
         </div>
       ) : null}
       {!rockets.loading && rockets.rocket.length ? (
-        rockets.rocket.map((rock) => (
-          <div key={rock.id} className=" d-flex-space g-10 my-20">
-            <img src={rock.flickr_images} className="logo" alt="rocket" />
-            <div className="rocket-details">
-              <h2 className="rocket-name">{rock.name}</h2>
-              <p className="rocket-desc">{rock.description}</p>
-              <button type="button" className="rocket-reserve btn">Reserve Rocket</button>
-            </div>
-          </div>
+        rockets.rocket.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            name={rocket.name}
+            img={rocket.flickr_images}
+            id={rocket.id}
+            desc={rocket.description}
+          />
         ))
       ) : null}
     </div>

78f15efb6521d48b3333d7cdf07617b854cb729c
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-06-22 10:55:08 +0300

    Style Added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './features/Rockets/RocketSlice';
 import MissionSlice from './missionSlice';
+
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
